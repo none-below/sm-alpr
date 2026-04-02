b513d73e1db0be75e60e5c6d7cb2f8d1be910380
bc <bc@zerobelow.org>
2026-04-02 01:00:57 -0700

    Fixing map load

--- docs/js/map.js
+++ docs/js/map.js
@@ -6,7 +6,7 @@ const SLUG_RE = /^[a-z0-9][a-z0-9\-]*$/;
 function safeSlug(s) { return SLUG_RE.test(s) ? s : ''; }
 
 // Load data
-fetch('data/map_data.json?v=1775115979').then(r => r.json()).then(data => {
+fetch('data/map_data.json?v=1775116755').then(r => r.json()).then(data => {
   const markers = data.markers;
   const coords = data.coords;
   const agencyInfo = data.agencyInfo;
